060be400587fd98d404771e5e3f023a57023472f
Suchita Mishra <suchitamishra318@gmail.com>
2023-01-19 10:05:41 +0000

    FIX : Added utilities

---
+++ utils/fetchBestSeller.ts
@@ -0,0 +1,31 @@
+export async function fetchBestSeller() {
+  const query = `query{
+  productCollection(where:{bestSeller:true}){
+    items{
+      slug
+      title
+      price
+      productImagesCollection{
+        items{
+          url
+          description
+        }
+      }
+    }
+  }
+}`;
+
+  const response = await fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.SPACE_ID}/environments/master`,
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+      },
+      body: JSON.stringify({ query }),
+    }
+  ).then((res) => res.json());
+
+  return response.data.productCollection.items;
+}
